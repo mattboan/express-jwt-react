ebc684e59a84f04e87b7f4755dca816fea335a80
Matthew boan <mattboan97@gmail.com>
2021-01-28 22:10:16 +0800

    Goodnight

--- index.js
+++ index.js
@@ -4,7 +4,40 @@ const cookieParser = require("cookie-parser");
 const path = require("path");
 const jwt = require("jsonwebtoken");
 
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
 //Allow static access to the front-end
-app.use(express.static(path.join(__dirname, "front-end")));
+app.use(express.static(path.join(__dirname, "front-end/public")));
+
+//Match any route if a predefined route doesn't exist
+app.get("*", function (req, res) {
+	res.sendFile(path.join(__dirname, "front-end/public", "index.html"));
+});
+
+/**
+ * TODO:
+ *       - Add MySQL integration
+ *       - Check if user exists
+ *       - Add user, etc.
+ *       - Connect on the front end
+ */
+app.post("/api/register", function (req, res) {
+	const { email, password } = req.body;
+
+	user.save(function (err) {
+		if (err) {
+			console.log(err);
+			res.status(500).send(
+				"Error registering new user please try again."
+			);
+		} else {
+			res.status(200).send("Welcome to the club!");
+		}
+	});
+});
 
 app.listen(process.env.PORT || 8080);
